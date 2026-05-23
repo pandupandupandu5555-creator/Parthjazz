c7d665cd2cefac80ae6c9f75a3a3f04fab5c47aa
pandupandupandu5555-creator <pandupandupandu5555@gmail.com>
2026-05-23 19:33:50 +0530

    Update main.tsx

--- artifacts/jarvis/src/main.tsx
+++ artifacts/jarvis/src/main.tsx
@@ -1,8 +1,9 @@
+
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { setBaseUrl } from "@workspace/api-client-react";
 
-setBaseUrl(import.meta.env.VITE_API_URL);
+setBaseUrl("https://workspaceapi-server-production-6165.up.railway.app/api");
 
 createRoot(document.getElementById("root")!).render(<App />);
